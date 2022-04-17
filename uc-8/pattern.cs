01a2c8a99f9c15629091b3bb1c5ff039868a379b
srikala <vlslaksh1899@gmail.com>
2022-04-17 14:19:30 +0530

    added ruleno.4 implemented program of uc-3 case

---
+++ uc-8/pattern.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace RegEx
+{
+    public class Patterns
+    {
+        public static void Password(string name)
+        {
+            string Password = "^.*(?=.{8,})(?=.*)(?=.*[a-z])(?=.*[A-Z])(?=.*[!*@#$%^&+=]).*$";
+            Regex regex = new Regex(Password);
+            if (regex.IsMatch(name))
+            {
+                Console.WriteLine(name + " is valid name");
+            }
+            else
+            {
+                Console.WriteLine(name + " is invalid.");
+            }
+        }
+    }
+}
