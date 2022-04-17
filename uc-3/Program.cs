931e81436e85767a11d16d73c92f1512a82b50b2
srikala <vlslaksh1899@gmail.com>
2022-04-17 13:42:15 +0530

    added uc-3 program to the repository

---
+++ uc-3/Program.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+
+namespace RegEx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Regular Expression Program \n");
+            Console.WriteLine("Write your Email ex : example123@gmail.com your can write yahoo and any other mail ID.");
+            Console.Write("\n Enter Email : ");
+            string name = Console.ReadLine();
+            Patterns.Email(name);
+        }
+    }
+}
---
+++ uc-3/pattern.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace RegEx
+{
+    public class Patterns
+    {
+        public static void Email(string name)
+        {
+            string Email = "^[A-Z0-9a-z]{1,}([.#$^][A-Za-z0-9]+)*[@][A-Za-z]{2,}[.][A-Za-z]{2,3}([.][a-zA-Z]{2})?$";
+            Regex regex = new Regex(Email);
+            if (regex.IsMatch(name))
+            {
+                Console.WriteLine(name + " is valid name");
+            }
+            else
+            {
+                Console.WriteLine(name + " is invalid.");
+            }
+        }
+    }
+}
