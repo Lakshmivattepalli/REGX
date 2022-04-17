77274456b9abe263959e06279adec5167f3a9f0b
srikala <vlslaksh1899@gmail.com>
2022-04-17 14:23:28 +0530

    added uc-9 program of combining all rules to repo

---
+++ uc-9/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace uc_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
