7a433ecaa5e0671ff9617f230b669adcd66ad952
srikala <vlslaksh1899@gmail.com>
2022-04-17 13:54:02 +0530

    added ruleno.1 password code of uc-5

---
+++ uc-5/pattern.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace RegEx
+{
+    public class Patterns
+    {
+        public static void Password(string name)
+        {
+            string Password = "^[A-Za-z0-9]{8,}";
+            Regex regex = new Regex(Password);
+            if (regex.IsMatch(name))
+            {
+                Console.WriteLine(name + " is valid name");
+            }
+            else
+            {
+                Console.WriteLine(name + " is invalid.");
+            }
+        }
+    }
+}
